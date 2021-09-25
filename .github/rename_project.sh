4d8299c7f52a7dfc7231f324d2208472e01d6b12
astdarkness <astdarkness@users.noreply.github.com>
2021-09-25 16:50:30 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="astdarkness"
+original_name="alan_api"
+original_urlname="alan-api"
+original_description="Awesome alan_api created by astdarkness"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv alan_api $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
